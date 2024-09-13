d10cffdc6e628fbc8186d09a1e419ea58452a989
swapnils118 <swapnil118@icloud.com>
2024-09-13 12:14:36 -0400

    Tailwind styling added to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,34 @@
 import Link from "next/link";
 import Logo from "./logo";
 
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-6">
       <Logo />
-      <Link href="/"> Home</Link>
-      <Link href="/events/all">All Events</Link>
+
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((routes) => (
+            <li
+              key={routes.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={routes.path}>{routes.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
