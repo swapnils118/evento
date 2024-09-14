f1d7ec0c7dcfee97eec362b7b09ed4c22d755c89
swapnils118 <swapnil118@icloud.com>
2024-09-14 13:41:49 -0400

    EventList component created

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,31 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events: EventoEvent[] = await response.json();
+  // console.log(events);
+
+  const city = params.city;
+
   return (
     <main className="flex flex-col items-center px-[20px] py-24 min-h-[110vh]">
-      <H1>Events in Austin</H1>
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <EventsList events={events} />
     </main>
   );
 }
