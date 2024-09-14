79c65fb9abba8be3a7aee736878f3549110b5fb2
swapnils118 <swapnil118@icloud.com>
2024-09-13 22:15:12 -0400

    Code modified

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,9 @@
+import H1 from "@/components/h1";
+
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  return (
+    <main className="flex flex-col items-center px-[20px] py-24 min-h-[110vh]">
+      <H1>Events in Austin</H1>
+    </main>
+  );
 }
