63590928f978ea1bc02814414b875c96632d08c7
swapnils118 <swapnil118@icloud.com>
2024-09-15 14:13:10 -0400

    Event styling

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function EventPage({ params }: EventPageProps) {
   console.log(event);
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative  overflow-hidden flex justify-center items-center py-12 md:py-20">
         <Image
           src={event.imageUrl}
           className="object-cover blur-3xl z-0"
@@ -27,19 +27,33 @@ export default async function EventPage({ params }: EventPageProps) {
           sizes="(max-width:1280px) 100vw,1280px"
           priority
         />
-        <div className="z-1 relative">
+        <div className="z-1 flex flex-col lg:flex-row relative gap-6 lg:gap-x-16">
           <Image
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
           />
 
-          <div>
-            <H1>{event.name}</H1>
-            <p>
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whiteSpace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-1 bg-blur focus:scale-105 hover:scale-105 active:scale-[1.02] transition">
+              Get tickets
+            </button>
           </div>
         </div>
       </section>
