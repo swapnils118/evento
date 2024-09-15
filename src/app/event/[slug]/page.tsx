28794a58553ab2da800ee397bc8ec1066d2ef6c6
swapnils118 <swapnil118@icloud.com>
2024-09-14 20:47:24 -0400

    Progress on event card

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,50 @@
-export default function EventPage() {
-  return <main>Event page</main>;
+import H1 from "@/components/h1";
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  console.log(event);
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={event.imageUrl}
+          className="object-cover blur-3xl z-0"
+          alt="eventImage"
+          fill
+          quality={50}
+          sizes="(max-width:1280px) 100vw,1280px"
+          priority
+        />
+        <div className="z-1 relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+
+          <div>
+            <H1>{event.name}</H1>
+            <p>
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,17 +9,17 @@ type EventsPageProps = {
 };
 
 export default async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
+
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventoEvent[] = await response.json();
   // console.log(events);
 
-  const city = params.city;
-
   return (
     <main className="flex flex-col items-center px-[20px] py-24 min-h-[110vh]">
-      <H1>
+      <H1 className="mb-28">
         {city === "all" && "All Events"}
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
