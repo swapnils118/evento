37ee0593e23911ee88978250249a1bd20c8dac0c
swapnils118 <swapnil118@icloud.com>
2024-09-15 16:30:57 -0400

    Refactored code for EventPage

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -51,14 +51,40 @@ export default async function EventPage({ params }: EventPageProps) {
             <p className="whitespace-nowrap text-xl text-white/75">
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
-            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-1 bg-blur focus:scale-105 hover:scale-105 active:scale-[1.02] transition">
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-1 bg-blur state-effects">
               Get tickets
             </button>
           </div>
         </div>
       </section>
 
-      <div></div>
+      <div className=" min-h-[75vh] text-center px-5 py-16">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className=" max-w-4xl mx-auto text-lg leading-8 text-white/75">
+      {children}
+    </p>
   );
 }
--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -12,7 +12,7 @@ export default function EventCard({ event }: EventCardProps) {
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
     >
-      <section className=" w-full h-full flex flex-col relative bg-white/[3%] rounded-xl overflow-hidden hover:scale-105 active:scale-[1.02] transition">
+      <section className=" w-full h-full flex flex-col relative bg-white/[3%] rounded-xl overflow-hidden state-effects">
         <Image
           src={event.imageUrl}
           alt={event.name}
