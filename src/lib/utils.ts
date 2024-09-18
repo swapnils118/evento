9f1e76a5a603d6ccd464cd4705a71be29ba43608
swapnils118 <swapnil118@icloud.com>
2024-09-17 22:42:29 -0400

    Events list sorted in ascending order by date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,6 +19,9 @@ export async function getEvents(city: string) {
     where: {
       city: city === "all" ? undefined : capitalize(city),
     },
+    orderBy: {
+      date: "asc",
+    },
   });
   return events;
 }
